0fcc3ede3da4e53622bd6c933b6040482e3bd07a
Francis Obi <supazuko@gmail.com>
2021-03-13 21:34:21 +0100

    chapter four exercise

--- src/tdd/Factorial.java
+++ src/tdd/Factorial.java
@@ -1,16 +1,63 @@
-package africa.semicolon.chapterFour;
-
-import java.util.Scanner;
-
+package tdd;
 public class Factorial {
 
     private int input;
+    private int product;
+    private double exponent;
+    private double exponentRaisedToPowerX;
 
     public void setInput(int input) {
-        this.input = input;
+        if(input > 0){
+            this.input = input;
+        }else
+            System.out.println("invalid input");
+
     }
 
     public int getInput() {
         return input;
+    }
+
+    public void calculateFactorial(int input) {
+        setInput(input);
+        product = 1;
+        input = getInput();
+        while(!(input < 1)){
+            product *= input;
+            input--;
+        }
+    }
+
+    public int getFactorial() {
+        return product;
+    }
+
+    public void calculateExponent(int input) {
+        setInput(input);
+        exponent = 1;
+        int counter = 1;
+        while(counter <= input){
+            calculateFactorial(counter);
+            exponent = exponent + (1.0 / getFactorial());
+            counter++;
+        }
+    }
+
+    public double getExponent() {
+        return exponent;
+    }
+
+    public void calculateExponentRaisedToPowerX(int input) {
+        setInput(input);
+        exponentRaisedToPowerX = 1;
+        int counter;
+        for (counter = 1; counter <= input; counter++){
+            calculateFactorial(counter);
+            exponentRaisedToPowerX = exponentRaisedToPowerX + (Math.pow(input, counter)/getFactorial());
+        }
+    }
+
+    public double getExponentRaisedToPowerX() {
+        return exponentRaisedToPowerX;
     }
 }
--- src/tdd/FactorialMain.java
+++ src/tdd/FactorialMain.java
@@ -2,7 +2,22 @@ package tdd;
 
 import java.util.Scanner;
 
-public class FactorialTest {
+public class FactorialMain {
     public static void main(String[] args) {
+        int input;
+        Factorial factorial = new Factorial();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number to calculate factorial, exponent and exponent raised to power x: ");
+        input = reader.nextInt();
+
+        factorial.calculateFactorial(input);
+        System.out.println(factorial.getFactorial());
+
+        factorial.calculateExponent(input);
+        System.out.println(factorial.getExponent());
+
+        factorial.calculateExponentRaisedToPowerX(input);
+        System.out.println(factorial.getExponentRaisedToPowerX());
+
     }
 }
