0c97b23c20a5fc7e7174a7cd5908315885e69000
Francis Obi <supazuko@gmail.com>
2021-03-19 21:16:42 +0100

    chapter four exercise

---
+++ src/africa/semicolon/chapterFour/CheckerboardPatternOfAsterisksTest.java
@@ -0,0 +1,8 @@
+package africa.semicolon.chapterFour;
+
+public class CheckerboardPatternOfAsterisksTest {
+    public static void main(String[] args) {
+        CheckerboardPatternOfAsterisks checkerBoard = new CheckerboardPatternOfAsterisks();
+        checkerBoard.buildCheckerBoardWithAsterisks(10);
+    }
+}
