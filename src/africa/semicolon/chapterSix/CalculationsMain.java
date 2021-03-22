54b8eb118f0f064589868b9e888f0c31affb59d0
Francis Obi <supazuko@gmail.com>
2021-03-22 21:42:17 +0100

    chapter six exercises

---
+++ src/africa/semicolon/chapterSix/CalculationsMain.java
@@ -0,0 +1,12 @@
+package africa.semicolon.chapterSix;
+
+public class CalculationsMain {
+    public static void main(String[] args) {
+        System.out.println(Calculations.isMultiple(40, 2)+ "\n");
+        System.out.println(Calculations.isEven(6816893)+ "\n");
+        System.out.println(Calculations.squareOfAsterisks(4) + "\n");
+        System.out.println(Calculations.circleArea(5) + "\n");
+        System.out.println(Calculations.minimum3(2.0, 5.8, 60.6) + "\n");
+        System.out.println(Calculations.hypotenuse(2,3));
+    }
+}
