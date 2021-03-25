68bafa6d60a5cd3d09f31b45ea30e77e6413a9b3
Francis Obi <supazuko@gmail.com>
2021-03-25 21:47:44 +0100

    chapter six exercise

--- src/africa/semicolon/chapterSix/Calculations.java
+++ src/africa/semicolon/chapterSix/Calculations.java
@@ -70,4 +70,13 @@ public class Calculations {
             }
         }return powerOfInteger;
     }
+
+    public static boolean isPerfect(int number) {
+        int total = 0;
+        for (int i = 1; i <= (number/2); i++) {
+            if(number % i == 0)
+                total += i;
+        }
+        return total == number;
+    }
 }
