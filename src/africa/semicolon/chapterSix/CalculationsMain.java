9f39ff248bd0764c7b7e58d6bf0a5ddc296f0b5a
Francis Obi <supazuko@gmail.com>
2021-03-25 21:48:40 +0100

    chapter six exercise

--- src/africa/semicolon/chapterSix/CalculationsMain.java
+++ src/africa/semicolon/chapterSix/CalculationsMain.java
@@ -10,5 +10,6 @@ public class CalculationsMain {
         System.out.println(Calculations.hypotenuse(2,3));
         System.out.println(Calculations.squareOfAnyCharacter(5, '#') + "\n");
         System.out.println(Calculations.integerPower(3, 3));
+        System.out.println(Calculations.isPerfect(100));
     }
 }
