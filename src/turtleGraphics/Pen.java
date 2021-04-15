1ce1d49f47d6e82cabe1571077b780357d5ea8c4
Francis Obi <supazuko@gmail.com>
2021-04-15 19:56:31 +0100

    turtle class exercise

---
+++ src/turtleGraphics/Pen.java
@@ -0,0 +1,14 @@
+package turtleGraphics;
+
+public class Pen {
+
+    private boolean isDown;
+
+    public void setIsUp(boolean isUp) {
+        isDown = !isUp;
+    }
+
+    public boolean isUp() {
+        return !isDown;
+    }
+}
