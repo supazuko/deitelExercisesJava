dd4f915a114a77ba3644703f27d1b6d2e1bf0419
Francis Obi <supazuko@gmail.com>
2021-05-27 20:23:45 +0100

    whot card exercise

---
+++ src/whotCardGame/WhotPlayer.java
@@ -0,0 +1,27 @@
+package whotCardGame;
+
+import whotCardGame.WhotCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WhotPlayer {
+    private String name;
+    private List<WhotCard> cards = new ArrayList<>();
+
+    public WhotPlayer(String playerName) {
+        name = playerName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addCard(WhotCard card) {
+        cards.add(card);
+    }
+
+    public int getNumberOfCards() {
+        return cards.size();
+    }
+}
