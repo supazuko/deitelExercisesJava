a9250cb16f8de6267949c1ed469c13ff6dbaf75f
Francis Obi <supazuko@gmail.com>
2021-06-12 19:13:56 +0100

    added player v computer play

--- src/tictactoe/Game.java
+++ src/tictactoe/Game.java
@@ -1,5 +1,6 @@
 package tictactoe;
 
+import java.security.SecureRandom;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,6 +9,7 @@ public class Game {
     private boolean gameWon;
     private boolean BoardFull;
     boolean lastValuePlayedWasX;
+    boolean computerHasPlayed;
     private int movesMade = 0;
 
     public Game() {
@@ -17,15 +19,18 @@ public class Game {
         this.lastValuePlayedWasX = false;
     }
 
-    public void move(int gridPosition) throws GameOverException, BoardFullException, AlreadyOccupiedException {
-        if (BoardFull){
+    public void move(int gridPosition) throws GameOverException, BoardFullException, AlreadyOccupiedException, ArrayIndexOutOfBoundsException {
+        if (BoardFull) {
             throw new BoardFullException();
+        }
+        if (gridPosition < 1 || gridPosition > 9) {
+            throw new ArrayIndexOutOfBoundsException();
         }
         int position = gridPosition - 1;
         int row = position / 3;
         int column = position % 3;
         Value[][] array = gameBoard.getGameArray();
-        if (array[row][column] != Value.EMPTY){
+        if (array[row][column] != Value.EMPTY) {
             throw new AlreadyOccupiedException();
         }
         if (lastValuePlayedWasX) {
@@ -35,21 +40,53 @@ public class Game {
             array[row][column] = Value.X;
             lastValuePlayedWasX = true;
         }
-        if (isGameWon()){
+        if (isGameWon()) {
             throw new GameOverException();
         }
-        if (movesMade < 9){ movesMade++;}
-        if (movesMade == 9){ BoardFull = true;}
+        if (movesMade < 9) {
+            movesMade++;
+        }
+        if (movesMade == 9) {
+            BoardFull = true;
+        }
     }
 
-    public boolean isGameWon(){
+    public void computerPlay() throws GameOverException, BoardFullException, AlreadyOccupiedException {
+        SecureRandom random = new SecureRandom();
+        if (BoardFull) {
+            throw new BoardFullException();
+        }
+        int row;
+        int column;
+        Value[][] array = gameBoard.getGameArray();
+        do {
+            int position = 1 + random.nextInt(9);
+            row = (position - 1) / 3;
+            column = (position - 1) % 3;
+        } while (array[row][column] != Value.EMPTY);
+
+        array[row][column] = Value.X;
+        computerHasPlayed = true;
+        lastValuePlayedWasX = true;
+        if (isGameWon()) {
+            throw new GameOverException();
+        }
+        if (movesMade < 9) {
+            movesMade++;
+        }
+        if (movesMade == 9) {
+            BoardFull = true;
+        }
+    }
+
+    public boolean isGameWon() {
         Value[][] array = gameBoard.getGameArray();
         for (int row = 0; row < array.length; row++) {
             Set<Value> values = new HashSet<>();
-            for (int column = 0; column < array[row].length ; column++) {
+            for (int column = 0; column < array[row].length; column++) {
                 values.add(array[row][column]);
             }
-            if(!values.contains(Value.EMPTY) && values.size() == 1){
+            if (!values.contains(Value.EMPTY) && values.size() == 1) {
                 return true;
             }
         }
@@ -58,36 +95,31 @@ public class Game {
             for (int row = 0; row < array.length; row++) {
                 values.add(array[row][column]);
             }
-            if(!values.contains(Value.EMPTY) && values.size() == 1){
+            if (!values.contains(Value.EMPTY) && values.size() == 1) {
                 return true;
             }
         }
         Set<Value> leftDiagonalValues = new HashSet<>();
-
         for (int i = 0; i < array.length; i++) {
-            int row = i;
-            int column = i;
-            leftDiagonalValues.add(array[row][column]);
+
+            leftDiagonalValues.add(array[i][i]);
         }
-        if(!leftDiagonalValues.contains(Value.EMPTY) && leftDiagonalValues.size() == 1){
+        if (!leftDiagonalValues.contains(Value.EMPTY) && leftDiagonalValues.size() == 1) {
             return true;
         }
-        int row = 2;
-        int column = 0;
         Set<Value> rightDiagonalValues = new HashSet<>();
 
-        for (; row >= 0 ; row--) {
-            for (; column < array.length; column++) {
-                rightDiagonalValues.add(array[row][column]);
-            }
+        for (int row = 2, column = 0; row >= 0 && column < array.length; row--, column++) {
+            rightDiagonalValues.add(array[row][column]);
         }
-        if(!rightDiagonalValues.contains(Value.EMPTY) && rightDiagonalValues.size() == 1){
+
+        if (!rightDiagonalValues.contains(Value.EMPTY) && rightDiagonalValues.size() == 1) {
             return true;
         }
         return false;
     }
 
-    public void displayBoard(){
+    public void displayBoard() {
         gameBoard.displayBoard();
     }
 }
