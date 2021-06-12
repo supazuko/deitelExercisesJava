195181658319a61aa1e1fe37c6943a3a1e323e05
Francis Obi <supazuko@gmail.com>
2021-06-12 19:15:55 +0100

    added player v computer play

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -3,31 +3,51 @@ package tictactoe;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlreadyOccupiedException, BoardFullException, GameOverException {
         Game game = new Game();
         Scanner scanner = new Scanner(System.in);
         boolean gameOver = false;
+        int position;
 
         while (!gameOver){
-            try{
+            try {
+                System.out.println("Computer play: ");
+                game.computerPlay();
                 game.displayBoard();
-                System.out.println("Enter a position to play: ");
-                int position = scanner.nextInt();
+                System.out.println("Enter a number between 1-9 to play: ");
+                position = scanner.nextInt();
+                game.move(position);
+                game.displayBoard();
+            }catch (ArrayIndexOutOfBoundsException e){
+                do {
+                    System.out.println("Invalid entry, try again: ");
+                    game.displayBoard();
+                    position = scanner.nextInt();
+
+                } while (position < 1 || position > 9);
                 game.move(position);
             }catch (GameOverException e){
                 if (game.lastValuePlayedWasX) {
-                    System.out.println("O won!");
                     game.displayBoard();
+                    System.out.println();
+                    System.out.println("X won!");
                 }else {
                     game.displayBoard();
-                    System.out.println("X won!");
+                    System.out.println();
+                    System.out.println("O won!");
                 }
                 gameOver = true;
             }catch (BoardFullException e){
-                System.out.println("It's a draw!");
                 gameOver = true;
+                game.displayBoard();
+                System.out.println();
+                System.out.println("It's a draw!");
             }catch (AlreadyOccupiedException e){
                 System.out.println("That position is already occupied");
+                game.displayBoard();
+                System.out.println("Enter a position to play: ");
+                position = scanner.nextInt();
+                game.move(position);
             }
         }
     }
